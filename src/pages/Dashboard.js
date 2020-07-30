e872557d4d0cf0efa3cfc7626aa3c929dff49acc
David Valles <davidtjvalles@gmail.com>
2020-07-30 15:45:21 -0700

    Set chart height dynamically

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,86 +4,25 @@ import { HorizontalBar } from "react-chartjs-2";
 import { NavBar } from "../components/NavBar";
 import { genWods, genLabels, genDatasets } from "../utils/libs";
 
-//const chartColors = {
-//red: "rgb(255, 99, 132)",
-//orange: "rgb(255, 159, 64)",
-//yellow: "rgb(255, 205, 86)",
-//green: "rgb(75, 192, 192)",
-//blue: "rgb(54, 162, 235)",
-//purple: "rgb(153, 102, 255)",
-//grey: "rgb(201, 203, 207)",
-//};
+const LIST_BASE_SIZE = 10;
+const CANVAS_BASE_HEIGHT = 250;
 
-//const wods = [
-//{
-//id: 100,
-//date: "01-01-2020",
-//exercises: [
-//{ id: 1, name: "squats", reps: 150 },
-////{ id: 2, name: "pushup", reps: 75 },
-//{ id: 3, name: "pullup", reps: 50 },
-//],
-//},
-//{
-//id: 200,
-//date: "02-0-2020",
-//exercises: [
-//{ id: 1, name: "squats", reps: 50 },
-//{ id: 2, name: "pushup", reps: 30 },
-//{ id: 3, name: "pullup", reps: 25 },
-//],
-//},
-//{
-//id: 300,
-//date: "03-01-2020",
-//exercises: [
-//{ id: 1, name: "squats", reps: 100 },
-//{ id: 2, name: "pushup", reps: 100 },
-//{ id: 3, name: "pullup", reps: 50 },
-//],
-//},
-//];
-//console.log(wods);
-
-//const data = {
-//labels: ["01-01", "02-01", "03-01"],
-//datasets: [
-//{
-//label: "squats",
-//backgroundColor: chartColors.red,
-//data: [150, 50, 100],
-//},
-//{
-//label: "pushups",
-//backgroundColor: chartColors.blue,
-//data: [0, 30, 100],
-//},
-//{
-//label: "pullups",
-//backgroundColor: chartColors.green,
-//data: [50, 25, 50],
-//},
-//],
-//};
-
-const wods = genWods(10);
+const wods = genWods(90);
 const data = {
-  labels: genLabels(wods),
-  datasets: genDatasets(wods),
+  labels: genLabels(wods).reverse(),
+  datasets: genDatasets(wods).reverse(),
 };
-
-//console.log(wods);
-//console.log(data);
 const options = {
-  title: {
-    display: true,
-    text: "Maximus Workouts",
+  legend: {
+    display: false,
   },
   tooltips: {
     mode: "index",
     intersect: false,
+    filter: (item) => parseInt(item.value, 10) > 0,
   },
   responsive: true,
+  maintainAspectRatio: false,
   scales: {
     xAxes: [
       {
@@ -101,13 +40,25 @@ const options = {
   },
 };
 
+const setCanvasHeight = (listSize) => {
+  let factor = 1;
+  while (listSize >= LIST_BASE_SIZE * factor) {
+    factor += 1;
+  }
+
+  return CANVAS_BASE_HEIGHT * factor;
+};
+
 export const Dashboard = () => {
   return (
     <>
       <NavBar />
       <div className="mx-auto">
         <Container>
-          <div className="py-5">
+          <div
+            className="py-5 position-relative"
+            style={{ height: `${setCanvasHeight(data.labels.length)}px` }}
+          >
             <HorizontalBar data={data} options={options} />
           </div>
         </Container>
